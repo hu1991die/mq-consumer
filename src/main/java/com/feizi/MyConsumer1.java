23945fdc7ae912b48fbea3699097780605248f3f
ruanpeng <ruanpengwj43467@touna.cn>
2018-08-02 20:15:59 +0800

    编写消费者测试demo

--- src/main/java/com/feizi/MyConsumer1.java
+++ src/main/java/com/feizi/MyConsumer1.java
@@ -6,6 +6,8 @@ import com.feizi.starter.core.RocketMqConsumerListener;
 import com.feizi.starter.entity.MessageData;
 import com.feizi.starter.entity.User;
 import com.feizi.starter.util.JsonUtils;
+import com.feizi.starter.util.StringUtils;
+import org.apache.rocketmq.common.message.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -16,15 +18,26 @@ import java.util.Objects;
  * Created by feizi on 2018/6/27.
  */
 @Component
-@RocketMqConsumer(topic = "feizi_topic1", consumerGroup = "my_consumer_group1")
-public class MyConsumer1 implements RocketMqConsumerListener<MessageData> {
+@RocketMqConsumer(topic = "feizi_topic", selectorExpress = "tagA", consumerGroup = "my_consumer_group1")
+public class MyConsumer1 implements RocketMqConsumerListener<Message> {
     private static final Logger LOGGER = LoggerFactory.getLogger(MyConsumer1.class);
 
     @Override
-    public boolean consume(MessageData message) {
+    public boolean consume(Message message) {
         LOGGER.info("==========================consumer start=====================");
 
         if(Objects.isNull(message)){
+            //接收到空消息，也表明此次消费成功
+            return true;
+        }
+        LOGGER.info("MyConsumer received message: {}", message);
+        LOGGER.info("消息主题topic: {}", message.getTopic());
+        LOGGER.info("消息子主题tags: {}", message.getTags());
+        LOGGER.info("消息keys: {}", message.getKeys());
+
+        //取出消息体
+        byte[] messageBody = message.getBody();
+        if(Objects.isNull(messageBody)){
             //接收到空消息，也表明此次消费成功
             return true;
         }
@@ -32,14 +45,15 @@ public class MyConsumer1 implements RocketMqConsumerListener<MessageData> {
         /**
          * TODO 具体的业务逻辑
          */
-        LOGGER.info("MyConsumer1 received message: {}", message);
-        LOGGER.info("UUID唯一值，用于消费幂等控制：: {}", message.getUuid());
-        LOGGER.info("消息产生时间戳: {}", message.getTimestamp());
-        LOGGER.info("消息内容: {}", message.getData());
+
+        String messageStr = StringUtils.byteArr2Str(messageBody);
+        MessageData<User> messageData = JsonUtils.jsonStr2Obj(messageStr, new TypeReference<MessageData<User>>(){});
+        LOGGER.info("UUID唯一值，用于消费幂等控制：: {}", messageData.getUuid());
+        LOGGER.info("消息产生时间戳: {}", messageData.getTimestamp());
 
         //json字符串转Obj
-        User user = JsonUtils.jsonObj2Obj(message.getData(), new TypeReference<User>(){});
-        LOGGER.info("具体内容: {}", user);
+        User user = JsonUtils.jsonObj2Obj(messageData.getData(), new TypeReference<User>(){});
+        LOGGER.info("消息内容: {}", user);
         LOGGER.info("id: {}", user.getId());
         LOGGER.info("name: {}", user.getName());
         LOGGER.info("age: {}", user.getAge());
--- src/main/java/com/feizi/MyConsumer2.java
+++ src/main/java/com/feizi/MyConsumer2.java
@@ -6,6 +6,8 @@ import com.feizi.starter.core.RocketMqConsumerListener;
 import com.feizi.starter.entity.MessageData;
 import com.feizi.starter.entity.User;
 import com.feizi.starter.util.JsonUtils;
+import com.feizi.starter.util.StringUtils;
+import org.apache.rocketmq.common.message.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -16,15 +18,26 @@ import java.util.Objects;
  * Created by feizi on 2018/6/27.
  */
 @Component
-@RocketMqConsumer(topic = "feizi_topic2", consumerGroup = "my_consumer_group2")
-public class MyConsumer2 implements RocketMqConsumerListener<String> {
+@RocketMqConsumer(topic = "feizi_topic", selectorExpress = "tagB", consumerGroup = "my_consumer_group1")
+public class MyConsumer2 implements RocketMqConsumerListener<Message> {
     private static final Logger LOGGER = LoggerFactory.getLogger(MyConsumer2.class);
 
     @Override
-    public boolean consume(String message) {
+    public boolean consume(Message message) {
         LOGGER.info("==========================consumer start=====================");
 
         if(Objects.isNull(message)){
+            //接收到空消息，也表明此次消费成功
+            return true;
+        }
+        LOGGER.info("MyConsumer received message: {}", message);
+        LOGGER.info("消息主题topic: {}", message.getTopic());
+        LOGGER.info("消息子主题tags: {}", message.getTags());
+        LOGGER.info("消息keys: {}", message.getKeys());
+
+        //取出消息体
+        byte[] messageBody = message.getBody();
+        if(Objects.isNull(messageBody)){
             //接收到空消息，也表明此次消费成功
             return true;
         }
@@ -32,15 +45,15 @@ public class MyConsumer2 implements RocketMqConsumerListener<String> {
         /**
          * TODO 具体的业务逻辑
          */
-        MessageData<User> messageData = JsonUtils.jsonStr2Obj(message, new TypeReference<MessageData<User>>(){});
-        LOGGER.info("MyConsumer1 received message: {}", messageData);
+
+        String messageStr = StringUtils.byteArr2Str(messageBody);
+        MessageData<User> messageData = JsonUtils.jsonStr2Obj(messageStr, new TypeReference<MessageData<User>>(){});
         LOGGER.info("UUID唯一值，用于消费幂等控制：: {}", messageData.getUuid());
         LOGGER.info("消息产生时间戳: {}", messageData.getTimestamp());
-        LOGGER.info("消息内容: {}", messageData.getData());
 
         //json字符串转Obj
         User user = JsonUtils.jsonObj2Obj(messageData.getData(), new TypeReference<User>(){});
-        LOGGER.info("具体内容: {}", user);
+        LOGGER.info("消息内容: {}", user);
         LOGGER.info("id: {}", user.getId());
         LOGGER.info("name: {}", user.getName());
         LOGGER.info("age: {}", user.getAge());
